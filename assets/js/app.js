bd926f8944869e0fbdf99002cbc5c345207755b7
sim001 <simon.amoyal4@gmail.com>
2020-09-14 15:09:52 +0200

    ajout page profil

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,13 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import '../css/register.css';
+import '../css/profil.css';
+
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
